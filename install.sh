00676811bdf379bbc808d0b7ad5ca38d1942f45e
Huu Nguyen <whoshuu@gmail.com>
2019-04-03 13:04:20 -0700

    Add additional replaces for extensible configs in install.sh

--- install.sh
+++ install.sh
@@ -26,3 +26,29 @@ ln -sfn ${DIR}/.fzf ~/.fzf
 ln -sf ${DIR}/.fzf.version ~/.fzf.version
 ln -sf ${DIR}/.fzf.bash ~/.fzf.bash
 ln -sf ${DIR}/.fzf.zsh ~/.fzf.zsh
+
+function replace() {
+    OLD=${1}
+    NEW=${2}
+    if [ -e ${OLD} ]; then
+        if ! diff -q ${NEW} ${OLD}; then
+            echo ""
+            read -p "        Are you sure you want to replace ${OLD} with ${NEW}? (y/n) " choice
+            case "$choice" in
+                y|Y ) echo -e "\n\tReplacing ${OLD} with ${NEW}" && cp ${OLD} ${OLD}.backup && cp ${NEW} ${OLD} && echo -e "\n\tBacked up original ${OLD} to ${OLD}.backup";;
+                n|N ) echo -e "\n\tIgnoring file";;
+                * ) echo -e "\n\tPlease type y or n, exiting" && exit 1;;
+            esac
+            echo ""
+        fi
+    else
+        cp ${NEW} ${OLD}
+    fi
+}
+
+echo -e "\nChecking for additional files...\n"
+
+replace ~/.gitconfig.user ${DIR}/.gitconfig.user
+replace ~/.Xresources.base16 ${DIR}/.Xresources.${XRESOURCES_COLORS_SCHEME:-seoul}
+
+echo -e "Final checks done, installation complete!\n"

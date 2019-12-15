5055ad7d518699d59610621aa3a0a7b8a260031b
Huu Nguyen <whoshuu@gmail.com>
2019-12-15 11:38:04 -0800

    Add reload function

---
+++ .config/i3/reload.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+xrandr --output DP-1-1 --auto --right-of DP-1-3 --mode 1920x1080
+feh --bg-scale /home/huu/.config/i3/bg.jpg
+#compton --config /home/huu/.config/compton/config -b
